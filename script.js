0075c0338c2f17ce7d65359e2a3df9802ef73d20
Chanchal-Choudhary <chanchalchoudhary.3022@gmail.com>
2026-05-13 19:25:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,8 +73,8 @@ function page2Animation() {
     scrollTrigger: {
       trigger: "#page2",
       scroller: "body",
-      start: "top 10%",
-      end: "top 120%",
+      start: "top top",
+      end: "top 90%",
       scrub: 2,
     }
   })
